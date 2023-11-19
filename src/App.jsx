20d2f63e6c51d7ffdf7e309016f1ab4eef414ebf
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-11-19 14:20:11 +0200

    Feed component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.scss";
-
 import Chat from "./components/Chat/Chat";
+import Feed from "./components/Feed/Feed";
+
+import "./App.scss";
 
 function App() {
 	return (
@@ -10,7 +11,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<div>Home</div>} />
 					<Route path="/profile" element={<div>profile</div>} />
-					<Route path="/feed" element={<div>feed</div>} />
+					<Route path="/feed" element={<Feed />} />
 					<Route path="/login" element={<div>login</div>} />
 					<Route path="/register" element={<div>register</div>} />
 					<Route path="/post" element={<div>post</div>} />
