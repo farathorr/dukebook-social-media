e40675ac7e14438b84d9c78c62df92936d0949cf
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-11-19 15:59:43 +0200

    Add route paths to navigation links

--- src/components/HeaderNav/HeaderNav.jsx
+++ src/components/HeaderNav/HeaderNav.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import style from "./HeaderNav.module.scss";
+import { Link } from "react-router-dom";
 
 export default function HeaderNav() {
 	return (
 		<nav className={style["header-nav"]}>
 			<img src="" alt="logo" />
-			<a href="">Lorem.</a>
-			<a href="">Facere?</a>
-			<a href="">Temporibus.</a>
+			<Link to="/">home</Link>
+			<Link to="/profile">profile</Link>
+			<Link to="/feed">feed</Link>
+			<Link to="/login">login</Link>
+			<Link to="/register">register</Link>
+			<Link to="/post">post</Link>
+			<Link to="/chat">chat</Link>
 		</nav>
 	);
 }
