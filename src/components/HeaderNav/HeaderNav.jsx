f8952913f97c101a41faa56e8cf23ea322ae5e36
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-11-26 13:57:25 +0200

    Add user key to tokens

--- src/components/HeaderNav/HeaderNav.jsx
+++ src/components/HeaderNav/HeaderNav.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import { useContext } from "react";
 import style from "./HeaderNav.module.scss";
 import { Link } from "react-router-dom";
 import ProfileIcon from "./ProfileIcon/ProfileIcon";
 import logo from "../../images/logo.png";
+import { AuthenticationContext } from "../AuthenticationControls/AuthenticationControls";
 
 export default function HeaderNav() {
+	const [authentication] = useContext(AuthenticationContext);
 	return (
 		<nav className={style["header-nav"]}>
 			<img src={logo} alt="logo" />
 			<div className={style.links}>
 				<Link to="/">home</Link>
-				<Link to="/user/nash">profile</Link>
+				{authentication.isAuthenticated && <Link to={`/user/${authentication.user.userTag}`}>profile</Link>}
 				<Link to="/feed">feed</Link>
 				<Link to="/post">post</Link>
 				<Link to="/chat">chat</Link>
--- src/components/HeaderNav/ProfileIcon/ProfileIcon.jsx
+++ src/components/HeaderNav/ProfileIcon/ProfileIcon.jsx
@@ -2,10 +2,11 @@ import style from "./ProfileIcon.module.scss";
 import { AuthenticationContext } from "../../AuthenticationControls/AuthenticationControls";
 import { useContext } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function ProfileIcon() {
 	const [authentication, setAuthentication] = useContext(AuthenticationContext);
+	const navigate = useNavigate();
 	if (!authentication.isAuthenticated)
 		return (
 			<Link to="/login" className={style["sign-in"]}>
@@ -32,5 +33,6 @@ export default function ProfileIcon() {
 
 		localStorage.removeItem("accessToken");
 		localStorage.removeItem("refreshToken");
+		navigate("/");
 	}
 }
