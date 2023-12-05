0ccbf0625bf85696b6ce8256ab1c35f4d586e1bd
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-12-05 18:20:02 +0200

    Chat links support

--- frontend/src/components/Chat/MessageRow/MessageRow.jsx
+++ frontend/src/components/Chat/MessageRow/MessageRow.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import style from "./MessageRow.module.scss";
 
 export default function MessageRow(props) {
@@ -8,12 +9,62 @@ export default function MessageRow(props) {
 				<span className={style["message-user-name"]}>{props.name} </span>
 				<span className={style["message-date"]}>{formatDate(props.date)}</span>
 				{props.messages.map((message, index) => (
-					<pre key={index}>{message.text}</pre>
+					<MessageText key={index} text={message.text} />
 				))}
 			</div>
 		</div>
 	);
 }
+
+function MessageText(props) {
+	const linkRegex = /(https?:\/\/[^ \n]+)/g;
+
+	return (
+		<pre>
+			{props.text.split(linkRegex).map((text, i) => {
+				if (i % 2 === 0) return <span key={i}>{text}</span>;
+				else return <LinkToImage key={i} link={text}></LinkToImage>;
+			})}
+		</pre>
+	);
+}
+
+const LinkToImage = ({ link }) => {
+	const image = new Image();
+	const [loaded, setLoaded] = useState(false);
+
+	useEffect(() => {
+		image.src = link;
+		image.onload = () => {
+			setLoaded(true);
+		};
+	}, []);
+
+	if (loaded) return <img className={style["image"]} src={link} alt="Image" />;
+	else return <a href={link}>{link}</a>;
+
+	// const [image, setImage] = useState(null);
+	// const [error, setError] = useState(false);
+
+	// useEffect(() => {
+	// 	const fetchImage = async () => {
+	// 		try {
+	// 			const response = await fetch(link);
+	// 			if (!response.ok) throw new Error("Image not found");
+	// 			const blob = await response.blob();
+	// 			setImage(URL.createObjectURL(blob));
+	// 		} catch (err) {
+	// 			setError(true);
+	// 		}
+	// 	};
+	// 	fetchImage();
+	// }, [link]);
+
+	// if (error) return null;
+	// if (!image) return <div className={style["image-placeholder"]} />;
+
+	// return <img className={style["image"]} src={image} alt="Image" />;
+};
 
 function formatDate(utcTime) {
 	if (!utcTime) return utcTime;
