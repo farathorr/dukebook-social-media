cba188b605020fcc17c666babf123b68be7574ea
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-12-09 16:18:04 +0200

    Post tags styling

--- frontend/src/components/Feed/NewPostForm/NewPostForm.jsx
+++ frontend/src/components/Feed/NewPostForm/NewPostForm.jsx
@@ -12,7 +12,6 @@ export default function NewPostForm({ setUpdatePostContent }) {
 	const { authentication } = useContext(AuthenticationContext);
 	const [postText, setPostText] = useState("");
 	const tagRef = createRef();
-	// const [tags, setTags] = useState("");
 	const [showTagInput, setShowTagInput] = useState(false);
 	const [tags, setTags] = useState([]);
 
@@ -23,16 +22,15 @@ export default function NewPostForm({ setUpdatePostContent }) {
 		if (postText.length < 1) return addNotification({ ...postError, message: "Post can't be empty" });
 
 		try {
-			const splitTags = tags.trim().length ? tags.split(" ") : [];
-			const post = { userTag: authentication.user.userTag, postText, tags: splitTags };
+			const formatedTags = [...new Set(tags.filter((tag) => tag.trim().replaceAll(" ", "")))];
+			const post = { userTag: authentication.user.userTag, postText, tags: formatedTags };
 			const { status, data } = await api.createPost(post);
 
 			if (status === 400) return addNotification({ ...postError, message: data.message });
 
 			setUpdatePostContent((state) => !state);
 			setPostText("");
-			setTags("");
-			setShowTagInput(false);
+			setTags([]);
 
 			addNotification({ type: "success", title: "Post successful", message: "Your post has been posted", duration: 3000 });
 		} catch (err) {}
@@ -64,7 +62,9 @@ export default function NewPostForm({ setUpdatePostContent }) {
 			<div
 				className={style["tag-input"]}
 				id="tag-input"
-				onClick={(e) => {
+				tabIndex="0"
+				placeholder="Enter tags"
+				onFocus={(e) => {
 					if (e.target.id === "tag-input") {
 						setTags((tags) => [...tags, ""]);
 						tagIndex = tags.length;
@@ -78,11 +78,16 @@ export default function NewPostForm({ setUpdatePostContent }) {
 						<input
 							className={style["tag-content"]}
 							value={tag}
+							autoComplete="off"
+							autoCorrect="off"
+							spellCheck="false"
+							autoCapitalize="off"
 							key={i}
 							ref={tagIndex === i ? tagRef : null}
 							onInput={(e) => {
 								const newTags = [...tags];
-								const splitTags = e.target.value.split(" ");
+								const value = e.target.value.replaceAll("#", "");
+								const splitTags = value.split(" ");
 								if (splitTags.length > 1) {
 									newTags[i] = splitTags[0];
 									newTags.splice(i + 1, 0, ...splitTags.slice(1));
@@ -90,8 +95,8 @@ export default function NewPostForm({ setUpdatePostContent }) {
 									setTags(newTags);
 									return;
 								} else {
-									newTags[i] = e.target.value;
-									setTags(newTags);
+									newTags[i] = value.replaceAll(" ", "");
+									setTags(newTags.filter((tag) => tag.length > 0));
 								}
 							}}
 						></input>
@@ -110,9 +115,6 @@ export default function NewPostForm({ setUpdatePostContent }) {
 					<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512">
 						<path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
 					</svg>
-				</button>
-				<button type="button" className={style["tags-button"]} onClick={() => setShowTagInput(!showTagInput)}>
-					{showTagInput ? "Close Tags" : "Add Tags"}
 				</button>
 			</div>
 		</form>
