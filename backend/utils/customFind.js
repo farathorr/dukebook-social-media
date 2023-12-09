4ec9633741d009812e2b000db2efcea5440f38ca
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-12-09 19:28:03 +0200

    Search with tags

--- backend/utils/customFind.js
+++ backend/utils/customFind.js
@@ -1,6 +1,22 @@
 function customFind(
 	schema,
-	{ limit, id, search, isComment, isOriginalPost, followedByUser, friendsWithUser, postByUserId, ...query } = {}
+	{
+		limit,
+		id,
+		search,
+		isComment,
+		isOriginalPost,
+		followedByUser,
+		friendsWithUser,
+		postByUserId,
+		filterLiked,
+		filterDisliked,
+		hasLiked,
+		countLikes,
+		hideLikes,
+		tags,
+		...query
+	} = {}
 ) {
 	const find = [{}];
 	const sort = { createdAt: -1, _id: 1 };
@@ -25,9 +41,35 @@ function customFind(
 		if (isComment) find[0].nestingLevel = { $gt: 0 };
 		else if (isOriginalPost) find[0].nestingLevel = 0;
 
-		if (followedByUser || friendsWithUser) find[0].$and = [];
+		if (followedByUser || friendsWithUser) find[0].$and ??= [];
 		if (followedByUser) find[0].$and.push({ user: { $in: followedByUser.followedIds } });
 		if (friendsWithUser) find[0].$and.push({ user: { $in: friendsWithUser.friendList } });
+
+		if (filterLiked || filterDisliked) find[0].$and ??= [];
+		if (filterLiked) find[0].$and.push({ likes: { $in: filterLiked } });
+		if (filterDisliked) find[0].$and.push({ dislikes: { $in: filterDisliked } });
+
+		if (hasLiked) {
+			find[1] ??= {};
+			schema.schema.eachPath((path) => (find[1][path] ??= true));
+
+			find[1].hasUserLiked = { $in: [hasLiked, "$likes"] };
+			find[1].hasUserDisliked = { $in: [hasLiked, "$dislikes"] };
+		}
+
+		if (countLikes) {
+			find[1] ??= {};
+			schema.schema.eachPath((path) => (find[1][path] ??= true));
+			find[1].likeCount = { $size: "$likes" };
+			find[1].dislikeCount = { $size: "$dislikes" };
+		}
+
+		if (tags) find[0].tags = { $in: tags };
+
+		if (hideLikes && find[1]) {
+			delete find[1].likes;
+			delete find[1].dislikes;
+		}
 
 		return schema
 			.find(...find)
