06c5320a93f0c2d9558238d030ad3639fde467fd
farathorr <aapo.sirius.salminen9@gmail.com>
2023-12-12 21:07:38 +0200

    add userPicture component to chat

--- frontend/src/pages/Chat/FriendRow/FriendRow.jsx
+++ frontend/src/pages/Chat/FriendRow/FriendRow.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import style from "./FriendRow.module.scss";
+import UserPicture from "../../../components/UserPicture/UserPicture";
 
 export default function FriendList({ group, setGroup }) {
 	return (
 		<div className={style["friend-row"]} onClick={() => setGroup(group)}>
-			<img className={style["profile-pic"]} src={group.image} alt="Profile picture" width={40} height={40} />
+			<UserPicture src={group.image} size="small" />
 			<div className={style["friend-info-container"]}>
 				<span className={style["user-name"]}>{group.name}</span>
 				<span className={style["user-last-message"]}>{group.lastMessage}</span>
