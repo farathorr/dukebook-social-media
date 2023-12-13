e422d889401c1c066b2c97bf716d44c1a1decedd
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-12-13 17:15:46 +0200

    Friend list styling

--- frontend/src/components/FriendList/FriendRow/FriendRow.jsx
+++ frontend/src/components/FriendList/FriendRow/FriendRow.jsx
@@ -4,30 +4,18 @@ import UserPicture from "../../../components/UserPicture/UserPicture";
 import UserTag from "../../../components/UserTag/UserTag";
 import { ChatGroupContext } from "../../../context/ChatGroupContext/ChatGroupContext";
 import { Link } from "react-router-dom";
+import CustomButton from "../../../components/CustomButton/CustomButton";
 
 export default function FriendList({ group }) {
 	const { setGroup } = useContext(ChatGroupContext);
 
-	if (group.type === "group") {
-		return (
-			<Link className={style["friend-row"]} to="/chat" onClick={() => setGroup(group)}>
-				<UserPicture src={group.image} size="small" />
-				<div className={style["friend-info-container"]}>
-					<span className={style["user-name"]}>{group.name}</span>
-					<span className={style["user-last-message"]}>{group.lastMessage}</span>
-				</div>
-			</Link>
-		);
-	} else {
-		return (
-			<Link className={style["friend-row"]} to="/chat" onClick={() => setGroup(group)}>
-				<UserPicture src={group.image} size="small" />
-				<div className={style["friend-info-container"]}>
-					<span className={style["user-name"]}>{group.name}</span>
-					<UserTag tag={group.name} isLink={true} />
-					<span className={style["user-last-message"]}>{group.lastMessage}</span>
-				</div>
-			</Link>
-		);
-	}
+	return (
+		<CustomButton className={style["friend-row"]} to="/chat" onClick={() => setGroup(group)}>
+			<UserPicture className={style["profile-pic"]} src={group.image} size="small" />
+			<div className={style["friend-info-container"]}>
+				<span className={style["user-name"]}>{group.name}</span>
+				{group.type === "chat" && <UserTag tag={group.name} isLink={true} />}
+			</div>
+		</CustomButton>
+	);
 }
--- frontend/src/components/UserPicture/UserPicture.jsx
+++ frontend/src/components/UserPicture/UserPicture.jsx
@@ -1,14 +1,15 @@
 import { useMemo } from "react";
 import style from "./UserPicture.module.scss";
 
-export default function UserPicture({ src, size }) {
+export default function UserPicture({ src, size = "small", className }) {
 	const key = src + size;
+	const classString = `${style[`profile-${size}-pic`]} ${className || ""}`;
 
 	const memorizedImage = useMemo(() => {
 		if (size === "big") {
-			return <img src={src || "https://i.imgur.com/XY5aZDk.png"} alt="ProfilePicture" className={style["profile-big-pic"]} />;
-		} else return <img src={src || "https://i.imgur.com/XY5aZDk.png"} alt="ProfilePicture" className={style["profile-small-pic"]} />;
+			return <img src={src || "https://i.imgur.com/XY5aZDk.png"} alt="ProfilePicture" className={classString} />;
+		} else return <img src={src || "https://i.imgur.com/XY5aZDk.png"} alt="ProfilePicture" className={classString} />;
 	}, [key]);
 
-	return <>{memorizedImage}</>;
+	return memorizedImage;
 }
